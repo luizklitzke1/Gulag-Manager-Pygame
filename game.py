e0e397e898e60d64ba82ef918d550639a53bcef4
luizklitzke1 <luiz.klitzke1@gmail.com>
2020-03-29 20:04:52 -0300

    Adicionados características básicas para a classe de campos

---
+++ game.py
@@ -0,0 +1,15 @@
+import pygame 
+
+
+pygame.init()
+
+S_WIDHT = 500
+S_HEIGHT = 500
+
+win = pygame.display.set_mode((S_WIDHT,S_HEIGHT))
+
+pygame.display.set_caption("Gulag Simulator")
+
+
+class Campo():
+    
