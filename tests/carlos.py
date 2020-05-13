6295d7ecd3157f0bc9f89a83769a5cc67c0025fe
Luiz Gustavo Klitzke <49824305+luizklitzke1@users.noreply.github.com>
2020-05-13 20:42:26 -0300

    Testes com sistema de um snippet

--- tests/carlos.py
+++ tests/carlos.py
@@ -1,1 +1,66 @@
-from general_functions import *
+#!/usr/bin/python3.4
+# Setup Python ----------------------------------------------- #
+import pygame, sys
+from pygame.locals import *
+import random
+import time
+import os
+
+fullscreen = False
+
+# Setup pygame/window ---------------------------------------- #
+mainClock = pygame.time.Clock()
+pygame.init()
+pygame.display.set_caption('Gulag Manager')
+icone = pygame.image.load("imgs/icone.png")
+pygame.display.set_icon(icone)
+SCREEN_WIDTH = 1920
+SCREEN_HEIGHT = 1080
+
+screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))#,FULLSCREEN | HWSURFACE | DOUBLEBUF)
+sh= screen.get_height()
+sw = screen.get_width() 
+
+from itertools import chain
+
+def truncline(text, font, maxwidth):
+        real=len(text)       
+        stext=text           
+        l=font.size(text)[0]
+        cut=0
+        a=0                  
+        done=1
+        old = None
+        while l > maxwidth:
+            a=a+1
+            n=text.rsplit(None, a)[0]
+            if stext == n:
+                cut += 1
+                stext= n[:-cut]
+            else:
+                stext = n
+            l=font.size(stext)[0]
+            real=len(stext)               
+            done=0                        
+        return real, done, stext             
+        
+def wrapline(text, font, maxwidth): 
+    done=0                      
+    wrapped=[]                  
+                               
+    while not done:             
+        nl, done, stext=truncline(text, font, maxwidth) 
+        wrapped.append(stext.strip())                  
+        text=text[nl:]                                 
+    return wrapped
+
+
+def wrap_multi_line(text, font, maxwidth):
+    """ returns text taking new lines into account.
+    """
+    lines = chain(*(wrapline(line, font, maxwidth) for line in text.splitlines()))
+    return list(lines)
+
+pygame.init() 
+font=pygame.font.Font(None, 17)
+print (wrapline("Now is the time for all good      men to come to the aid of their country", font, 120))
