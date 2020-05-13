07806806f269f90fcefbd029441b6fa87cb6817d
Luiz Gustavo Klitzke <49824305+luizklitzke1@users.noreply.github.com>
2020-05-12 23:07:29 -0300

    Desenv de teste com getattr e setattr para os upgrades

--- carlos.py
+++ carlos.py
@@ -1,1 +1,32 @@
-[ print(n) for n in range(1,11) ]
+class Cobaia():
+    
+    def __init__(self):
+        self.a = "a"
+        self.b = B()
+        
+class B():
+    
+    def __init__(self):
+        self.c = "C"
+
+class Teste():
+    
+    def __init__(self):
+        self.a = "A"
+        self.effects = [ 
+            ["a","a"]
+        ]
+        
+    def apply_effect(self,cobaia):
+        
+        for effect in self.effects:
+            new = getattr(cobaia,effect[0]) + effect[1]
+            setattr(cobaia,effect[0],new)
+
+
+cobaia = Cobaia()
+print(cobaia.a)
+teste = Teste()
+teste.apply_effect(cobaia)
+print(cobaia.a)
+        
--- upgrades.py
+++ upgrades.py
@@ -1,7 +1,7 @@
 #Classe geral - abstrata - para base dos upgrades
 class Upgrade():
     
-    def __init__(self,name,div,desc,price,effec):
+    def __init__(self,name,div,desc,price,effect,effect_txt):
         
         self.name = name
         self.div = div
@@ -9,8 +9,20 @@ class Upgrade():
         self.price = price
         self.effec = effec
         
+    def apply_effec(self,est,effect):
+        
+    
+        
 ##---------------------[Declaração dos upgrades em si] ---------------------##
 
 def upg_Analgesicos(Upgrade):
     
     def __init__(self):
+        
+        super().__init__("Analgésicos",medico,
+                         "Redução no tempo de recuperação de feridos",500,
+                         
+                         (("-20% tempo de recuperação",1)))
+        
+
+upg_list =
